450293a143e2f66542bfe8aaea08144eb65b5353
Jibin <jibinbabu966@gmail.com>
2025-07-29 22:05:50 +0530

    base case for empty strings

---
+++ src/stringCalculator.ts
@@ -0,0 +1,5 @@
+export function add(numbers: string): number {
+    // If the input is an empty string, return 0
+    if (numbers === "") return 0;
+    return 0; // for next step, we will implement the logic to handle numbers
+}
