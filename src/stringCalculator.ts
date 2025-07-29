666e6befc601de8a7d2f2cac395cff0e90865b5c
Jibin <jibinbabu966@gmail.com>
2025-07-29 22:50:38 +0530

    Refactor add function to normalize input handling and enhance test coverage for new line delimiters

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -2,8 +2,10 @@ export function add(numbers: string): number {
     // If the input is an empty string, return 0
     if (numbers === "") return 0;
     
+    // handle new lines as delimiters in addition to commas
+    const normalizedNumbers = numbers.replace(/\n/g, ",");
     // Split the string by commas to handle multiple numbers
-    const parts = numbers.split(",").map(Number);
+    const parts = normalizedNumbers.split(",").map(Number);
     // if number is a single number, return it as an integer
     if (parts.length === 1) return parts[0];
 
